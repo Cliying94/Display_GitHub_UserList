5630daa050a4f6ec9d01b04f4c9aeea289b4536d
Ticno <paticnoah@gmail.com>
2022-03-28 15:41:44 +0800

    Add project files.

---
+++ Display_GitHub_UserList/Pages/GitHubUserList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Display_GitHub_UserList.Pages
+{
+    public class GitHubUserListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
